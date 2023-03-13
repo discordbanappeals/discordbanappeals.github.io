2f8f7dc568e822b08d20d2f19fde8984ffff75ac
Glitch (glitch-hello-website) <none>
2023-03-13 14:03:25 +0000

    🎱👜 Checkpoint ./servers.json:39255183/5799:39744758/1641 ./index.html:39255183/189 ./servers.js:39744758/4557

--- servers.js
+++ servers.js
@@ -12,8 +12,39 @@ fetch("servers.json")
     // Hide the loader when the servers data is loaded
     loader.style.display = "none";
 
-    // Loop through each server in the server data and create a server listing
-    data.servers.forEach(server => {
+    // Separate premium and standard servers
+    const premiumServers = data.servers.filter(server => server.isPremium);
+    const standardServers = data.servers.filter(server => !server.isPremium);
+
+    // Loop through each premium server and create a server listing
+    premiumServers.forEach(server => {
+      console.log("Added" + server.name)
+      const serverItem = document.createElement("a"); // create an anchor element to wrap each server listing
+      serverItem.href = server.appealUrl; // set the URL of the anchor element to the server's appeal URL
+      serverItem.classList.add("server premium"); // add the "server" and "premium" CSS classes to the anchor element
+
+      // Set the inner HTML of the anchor element to the server listing
+      serverItem.innerHTML = `
+        <div class="server-icon">
+          <img src="${server.icon}" alt="Server Icon" />
+        </div>
+        <div class="server-details">
+          <h2 class="server-name">${server.name}</h2>
+          <p class="server-description">${server.description}</p>
+        </div>
+        <div class="server-action-box">
+          <div class="server-action">
+            <span class="server-action-label">Appeal</span>
+          </div>
+        </div>
+      `;
+
+      // Add the server listing to the server list
+      serverList.appendChild(serverItem);
+    });
+
+    // Loop through each standard server and create a server listing
+    standardServers.forEach(server => {
       const serverItem = document.createElement("a"); // create an anchor element to wrap each server listing
       serverItem.href = server.appealUrl; // set the URL of the anchor element to the server's appeal URL
       serverItem.classList.add("server"); // add the "server" CSS class to the anchor element
